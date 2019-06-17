fd436cbd210a8fcd3cfda7a1c589a9393086e283
user2684 <user2684@users.noreply.github.com>
2019-06-17 19:18:47 +0200

    Implemented all the requests

--- service/openweathermap.py
+++ service/openweathermap.py
@@ -9,7 +9,7 @@
 ## COMMUNICATION:
 # INBOUND: 
 # - IN: 
-#   required: request ("temperature", "pressure", "humidity", "wind", "condition", "description"), latitude, longitude
+#   required: request (temperature|pressure|humidity|wind|wind_dir|condition|description|visibility|rain_1h|rain_3h|snow_1h|snow_3h|clouds|uv|forecast_*), latitude, longitude
 #   optional: 
 # OUTBOUND: 
 
@@ -40,9 +40,15 @@ class Openweathermap(Service):
         self.add_configuration_listener(self.fullname, True)
 
     # map between user requests and openweathermap requests
-    def get_request_type(self,request):
-        if request in ["temperature", "pressure", "humidity", "wind", "condition", "description"]: return "weather"
-        return None
+    def get_request(self, request):
+        if request.startswith("forecast_"): return "forecast"
+        elif request == "uv": return "uvi"
+        else: return "weather"
+        
+    # set the value and timestamp to the message
+    def set_measure(self, message, value, timestamp):
+        message.set("value", value)
+        message.set("timestamp", self.date.timezone(int(timestamp)))
     
     # What to do when running    
     def on_start(self):
@@ -59,15 +65,15 @@ class Openweathermap(Service):
             sensor_id = message.args
             request = message.get("request")
             location = "lat="+str(message.get("latitude"))+"&lon="+str(message.get("longitude"))
-            if self.get_request_type(request) is None:
+            if self.get_request(request) is None:
                 self.log_error("invalid request "+request)
                 return
             # if the raw data is cached, take it from there, otherwise request the data and cache it
-            cache_key = "/".join([location, self.get_request_type(request)])
+            cache_key = "/".join([location, self.get_request(request)])
             if self.cache.find(cache_key): 
                 data = self.cache.get(cache_key)
             else:
-                url = self.url+"/"+self.get_request_type(request)+"?APPID="+self.config["api_key"]+"&units="+self.units+"&lang="+self.language+"&"+location
+                url = self.url+"/"+self.get_request(request)+"?APPID="+self.config["api_key"]+"&units="+self.units+"&lang="+self.language+"&"+location
                 try:
                     data = sdk.python.utils.web.get(url)
                 except Exception,e: 
@@ -83,41 +89,73 @@ class Openweathermap(Service):
             if not "cod" in parsed_json:
                 self.log_error("JSON missing 'cod': "+str(parsed_json))
                 return
-            if parsed_json["cod"] != 200:
-                self.log_error(parsed_json["message"])
+            if int(parsed_json["cod"]) != 200:
+                self.log_error("Invalid response: "+str(parsed_json["message"]))
                 return
             # reply to the requesting module 
             message.reply()
-            # handle the request
-            if request == "temperature":
-                message.set("value", float(parsed_json["main"]["temp"]))
-                message.set("timestamp", self.date.timezone(int(parsed_json["dt"])))
-            elif request == "humidity":
-                message.set("value", float(parsed_json["main"]["humidity"]))
-                message.set("timestamp", self.date.timezone(int(parsed_json["dt"])))
-            elif request == "wind":
-                message.set("value", float(parsed_json["wind"]["speed"]))
-                message.set("timestamp", self.date.timezone(int(parsed_json["dt"])))
-            elif request == "pressure":
-                message.set("value", float(parsed_json["main"]["pressure"]))
-                message.set("timestamp", self.date.timezone(int(parsed_json["dt"])))
-            elif request == "condition":
-                code = parsed_json["weather"][0]["main"]
-                value = "question"
-                if code in ["Thunderstorm"]: value = "cloud-showers-heavy"
-                elif code in ["Drizzle", "Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Ash", "Squall", "Tornado"]: value = "smog"
-                elif code in ["Rain"]: value = "cloud-rain"
-                elif code in ["Snow"]: value = "snowflake"
-                elif code in ["Clear"]: value = "sun"
-                elif code in ["Clouds"]: value = "cloud"
-                message.set("value", value)
-                message.set("timestamp", self.date.timezone(int(parsed_json["dt"])))
-            elif request == "description":
-                message.set("value", parsed_json["weather"][0]["description"])
-                message.set("timestamp", self.date.timezone(int(parsed_json["dt"])))
-            # TODO: add other capabilities
-            # send the response back
-            self.send(message)
+            entries = []
+            # if a forecast request, multiple entries are returned
+            if request.startswith("forecast_"):
+                request = request.replace("forecast_", "")
+                entries = parsed_json["list"]
+            # if a request for current condition, a single request is returned
+            else:
+                entries = [parsed_json]
+            # for each entry
+            for entry in entries:
+                if request == "temperature":
+                    self.set_measure(message, entry["main"]["temp"], entry["dt"])
+                elif request == "humidity":
+                    self.set_measure(message, entry["main"]["humidity"], entry["dt"])
+                elif request == "wind":
+                    self.set_measure(message, entry["wind"]["speed"], entry["dt"])
+                elif request == "wind_dir":
+                    degrees = entry["wind"]["deg"]
+                    direction = "question"
+                    if direction >= 315 or direction <= 45: direction = "arrow-down"
+                    elif direction > 45 and direction <= 135: direction = "arrow-left"
+                    elif direction > 135 and direction <= 225: direction = "arrow-up"
+                    elif direction > 225 and direction <= 315: direction = "arrow-right"
+                    self.set_measure(message, direction, entry["dt"])
+                elif request == "pressure":
+                    self.set_measure(message, entry["main"]["pressure"], entry["dt"])
+                elif request == "visibility":
+                    self.set_measure(message, entry["visibility"], entry["dt"])
+                elif request == "rain_1h":
+                    value = entry["rain"]["1h"] if "rain" in entry and "1h" in entry["rain"] else 0
+                    self.set_measure(message, value, entry["dt"])
+                elif request == "rain_3h":
+                    value = entry["rain"]["3h"] if "rain" in entry and "3h" in entry["rain"] else 0
+                    self.set_measure(message, value, entry["dt"])
+                elif request == "snow_1h":
+                    value = entry["snow"]["1h"] if "snow" in entry and "1h" in entry["snow"]  else 0
+                    self.set_measure(message, value, entry["dt"])
+                elif request == "snow_3h":
+                    value = entry["snow"]["3h"] if "snow" in entry and "3h" in entry["snow"]  else 0
+                    self.set_measure(message, value, entry["dt"])
+                elif request == "clouds":
+                    self.set_measure(message, entry["clouds"]["all"], entry["dt"])
+                elif request == "uv":
+                    self.set_measure(message, entry["value"], entry["date"])
+                elif request == "condition":
+                    code = entry["weather"][0]["main"]
+                    value = "question"
+                    if code in ["Thunderstorm"]: value = "cloud-showers-heavy"
+                    elif code in ["Drizzle", "Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Ash", "Squall", "Tornado"]: value = "smog"
+                    elif code in ["Rain"]: value = "cloud-rain"
+                    elif code in ["Snow"]: value = "snowflake"
+                    elif code in ["Clear"]: value = "sun"
+                    elif code in ["Clouds"]: value = "cloud"
+                    self.set_measure(message, value, entry["dt"])
+                elif request == "description":
+                    self.set_measure(message,  entry["weather"][0]["description"], entry["dt"])
+                    message.set("value", entry["weather"][0]["description"])
+                else: 
+                    self.log_error("invalid request: "+message.get("request"))
+                    return
+                # send the response back
+                self.send(message)
 
     # What to do when receiving a new/updated configuration for this module
     def on_configuration(self,message):
